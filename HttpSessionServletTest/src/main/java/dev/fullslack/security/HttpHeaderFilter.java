a788844276319f65a938b62f8e9fd9fcdf332d6e
Bart L <loeffen@hotmail.com>
2019-07-24 17:45:18 +0200

    Tweaked CSP header.

--- HttpSessionServletTest/src/main/java/dev/fullslack/security/HttpHeaderFilter.java
+++ HttpSessionServletTest/src/main/java/dev/fullslack/security/HttpHeaderFilter.java
@@ -12,7 +12,7 @@ public class HttpHeaderFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws ServletException, IOException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'");
+        httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://www.w3.com; object-src 'none'; form-action 'self'");
         httpServletResponse.setHeader("Referrer-Policy", "no-referrer");
         httpServletResponse.setHeader("Feature-Policy", "camera 'none'");
 
